f15ed396031bea95efec933cd26dbc88b58431b9
Jirko <hiei216@gmail.com>
2025-03-16 02:32:58 +0100

    Add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-  ],
+  plugins: [react(), svgr(), sentryVitePlugin({
+    org: "jirko",
+    project: "skf-prayer-app"
+  })],
+
   server: {
     open: true, // Open the browser on server start
+  },
+
+  build: {
+    sourcemap: true
   }
 });
