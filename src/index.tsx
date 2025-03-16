5b427915a57c1d4715b969e55f9e2f8902c39470
Jirko <hiei216@gmail.com>
2025-03-16 01:35:50 +0100

    Install and initialize sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import * as Sentry from "@sentry/react";
 
 import App from "./App.tsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://f6642b0b1cad8c28b32553ef03dde344@o4508978855870464.ingest.de.sentry.io/4508984607047760"
+});
 
 const queryClient = new QueryClient();
 
